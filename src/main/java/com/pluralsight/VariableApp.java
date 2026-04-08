e8fb15793bc74e12ae8a3ba2659a9514a42d170b
Gabe <Carrollgbusiness@gmail.com>
2026-04-08 14:52:21 -0500

    Created Variables

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        /* Declare Variables
+        code solution
+        display results
+         */
+        String favColor = "Black";
+        int classStart = 2026;
+        String midInit = "A.";
+        boolean havePet = true;
+        String niceMsg = "A Nice message or saying";
+
+    }
 }
